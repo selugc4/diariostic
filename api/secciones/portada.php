8c00a06b63cc335fdc46a7a8bdd4de629459c2d5
selugc4 <72543813+selugc4@users.noreply.github.com>
2024-10-22 19:18:19 +0200

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "La portada CC2022",
-"autor" => "mtorres",
-"resumen" => "El resumen maravilloso",
+"titulo" => "La portada actualizada",
+"autor" => "jgc450",
+"resumen" => "Resumen de la portada",
 ];
 ?>
